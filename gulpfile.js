72332db73bd52708cef18bdaaef535cb846adfc0
wjh <787688747.qq.com>
2020-10-20 19:25:09 +0800

    放大镜啥的

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 
-var { src , dest , series , parallel , watch} = require('gulp')
+var { src , dest , series , parallel , watch } = require('gulp');
 var clean = require('gulp-clean');
 var fileInclude = require('gulp-file-include');
 var webserver = require('gulp-webserver');
 var sass = require('gulp-sass');
-
+var cssmin = require('gulp-cssmin');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var requirejsOptimize = require('gulp-requirejs-optimize');
 
 function cleanTask(){
     return src('./dist' , {allowEmpty : true})
@@ -21,12 +24,18 @@ function fileIncludeTask(){
 }
 
 function webserverTask(){
-    return src('./dist')
+    return src('./dist')      //是view文件夹下作为localhost根目录
             .pipe( webserver({
                 host : 'localhost',
                 port : 4000,
                 open : './view/index.html',
                 livereload : true,
+                proxies:[   //配置反向代理
+                    {
+                        source:'/api2',
+                        target:'http://localhost/api2'
+                    }
+                ]
             }));
 }
 
@@ -41,17 +50,14 @@ function staticTask(){   //把src中的静态资源同步到dist下
             .pipe(dest('./dist/static'));
 }
 
-
 function libTask(){
     return src('./src/lib/**')
             .pipe( dest('./dist/lib') );
 }
-
 function apiTask(){
     return src('./src/api/**')
             .pipe( dest('./dist/api') );
 }
-
 function jsTask(){
     return src('./src/js/**')
             .pipe( dest('./dist/js') );
@@ -59,16 +65,38 @@ function jsTask(){
 
 function watchTask(){   //监听文件变化，同步到dist文件下
     watch('./src/view/**' , fileIncludeTask);
-    watch('./src/view/**' , sassTask);
+    watch('./src/css/**' , sassTask);
     watch('./src/static/**' , staticTask);
     watch('./src/lib/**' , libTask);
     watch('./src/api/**' , apiTask);
     watch('./src/js/**' , jsTask);
 }
 
+function buildCSSTask(){
+    return src('./src/css/*.scss')
+            .pipe(sass())
+            .pipe(cssmin())
+            .pipe(dest('./dist/css'));
+}
+
+function buildJSTask(){
+    return src('./src/js/*.js')
+            .pipe(requirejsOptimize({
+                optimize:"none",
+                paths:{                        
+                    "jquery":"empty:"  //不会把jquery模块合并进去
+                }
+            }))
+            .pipe(babel({
+                presets: ['es2015']
+             }))
+            .pipe(uglify())
+            .pipe( dest('./dist/js') );
+}
+
 module.exports = {
     // 开发环境下的命令
-    dev : series( cleanTask , parallel( fileIncludeTask , sassTask , staticTask , libTask , apiTask , jsTask), parallel(webserverTask , watchTask) ),    
+    dev : series( cleanTask , parallel(fileIncludeTask , sassTask , staticTask , libTask , apiTask , jsTask) , parallel(webserverTask , watchTask) ),    
     // 生产环境下的命令
-    build : series( cleanTask )
+    build : series( cleanTask , parallel(fileIncludeTask , buildCSSTask , buildJSTask , staticTask , libTask , apiTask) )
 };
--- src/api/server.js
+++ src/api/server.js
@@ -1,30 +1,55 @@
-
-
+//提供一个全站的所有api接口地址，通过{}提供给外面
 define(['jquery'],function($){
 
     function getBannerData(){
-        return $.ajax('/api/mock/banner.json');   //promise对象提供出去
+        return $.ajax('/api/mock/banner.json');  // promise对象提供出去
     }
     function getBanner2Data(){
-        return $.ajax('/api/mock/banner2.json');   //promise对象提供出去
+        return $.ajax('/api/mock/banner2.json');  // promise对象提供出去
     }
     function getPhoneData(){
-        return $.ajax('/api/mock/phone.json'); 
+        return $.ajax('/api/mock/phone.json');
     }
     function getBookData(){
-        return $.ajax('/api/mock/book.json'); 
+        return $.ajax('/api/mock/book.json');
     }
     function getPadData(){
-        return $.ajax('/api/mock/pad.json'); 
+        return $.ajax('/api/mock/pad.json');
     }
-    
+    function getDetailData(type , id){
+        var promise = new Promise(function(resolve,reject){
+            $.ajax(`/api/mock/${type}.json`).then((res)=>{
+                //console.log(res);
+                for(var i=0;i<res.goods_list.length;i++){
+                    if( id == res.goods_list[i].goodsId){
+                        //res.goods_list[i];   //找到id对象的数据
+                        resolve(res.goods_list[i]);
+                    }
+                }
+            });
+        });
+        return promise;
+    }
+
+    // 反向代理 php 页面
+    /* function actionLogin(){
+        return $.ajax('/api2/login.php'); //-> http://localhost/api2/login.php
+    }
+    actionLogin().then((res)=>{
+        console.log(res);
+    }).catch((err)=>{
+        console.log(err);
+    }); */
+
+
     return {
         getBannerData,
         getBanner2Data,
         getPhoneData,
         getBookData,
-        getPadData
-    }
-});
+        getPadData,
+        getDetailData
+    };
 
+});
 
